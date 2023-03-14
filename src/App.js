528bf748668ed2fbfb5cfe53fc1ef6ee034e2484
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-14 23:52:04 +0200

    add state and logic to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useState } from 'react';
 import './App.css';
+
+const questionPairs = [
+  {
+    this: "Dog",
+    that: "Cat"
+  }
+];
 
 function Navbar() {
   return (
@@ -8,32 +16,53 @@ function Navbar() {
   );
 }
 
-function Question() {
+function Question({ onButtonClick, selection }) {
+  const firstButtonText = questionPairs[0].this;
+  const secondButtonText = questionPairs[0].that;
+
   return (
     <div className="question-container">
-      <button className="question-btn">Dog</button>
+      <button 
+        className="question-btn" 
+        onClick={() => onButtonClick(firstButtonText)}>
+        {firstButtonText}
+      </button>
       <span className="question-text">OR</span>
-      <button className="question-btn">Cat</button>
+      <button 
+        className="question-btn" 
+        onClick={() => onButtonClick(secondButtonText)}>
+        {secondButtonText}
+      </button>
       <span className="question-text">?</span>
     </div>
   );
 }
 
-function Response() {
+function Response({ selection }) {
   return (
     <div className="response-container">
-      <p className="response-text">Dog</p>
+      {selection && <p className="response-text">You selected: <span>{selection}</span></p>}
     </div>
   );
 }
 
 function App() {
+  const [selected, setSelected] = useState(null);
+  // const [response, setResponse] = useState("Dog");
+
+  function handleButtonClick(buttonText) {
+    // console.log("clicked")
+    setSelected(buttonText);
+    console.log(selected);
+
+  }
+
   return (
     <div>
       <div className="App">
         <Navbar />
-        <Question />
-        <Response />
+        <Question onButtonClick={handleButtonClick} selection={selected} />
+        <Response selection={selected} />
       </div>
     </div>
   );
