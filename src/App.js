a3639c426abc89277ca3d2ad22e31c6ba44f21da
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-15 12:23:02 +0200

    completed static ui

--- src/App.js
+++ src/App.js
@@ -1,68 +1,122 @@
 import { useState } from 'react';
 import './App.css';
 
-const questionPairs = [
+const data = [
   {
-    this: "Dog",
-    that: "Cat"
+    "id": 1,
+    "optionA": "Pizza",
+    "optionB": "Tacos"
+  },
+  {
+    "id": 2,
+    "optionA": "Beach",
+    "optionB": "Mountains"
+  },
+  {
+    "id": 3,
+    "optionA": "Books",
+    "optionB": "Movies"
+  },
+  {
+    "id": 4,
+    "optionA": "Summer",
+    "optionB": "Winter"
+  },
+  {
+    "id": 5,
+    "optionA": "Coffee",
+    "optionB": "Tea"
+  },
+  {
+    "id": 6,
+    "optionA": "Cake",
+    "optionB": "Ice Cream"
+  },
+  {
+    "id": 7,
+    "optionA": "Dogs",
+    "optionB": "Cats"
+  },
+  {
+    "id": 8,
+    "optionA": "Guitar",
+    "optionB": "Piano"
+  },
+  {
+    "id": 9,
+    "optionA": "Cycling",
+    "optionB": "Running"
+  },
+  {
+    "id": 10,
+    "optionA": "Chocolate",
+    "optionB": "Vanilla"
   }
 ];
+
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">ThisOrThat</div>
+      <div className="logo">This||That</div>
     </nav>
   );
 }
 
-function Question({ onButtonClick, selection }) {
-  const firstButtonText = questionPairs[0].this;
-  const secondButtonText = questionPairs[0].that;
+function Question({ optionAText, optionBText, onButtonClick }) {
 
   return (
     <div className="question-container">
-      <button 
-        className="question-btn" 
-        onClick={() => onButtonClick(firstButtonText)}>
-        {firstButtonText}
+      <button
+        className="question-btn"
+        onClick={() => onButtonClick(optionAText)}>
+        {optionAText}
       </button>
       <span className="question-text">OR</span>
-      <button 
-        className="question-btn" 
-        onClick={() => onButtonClick(secondButtonText)}>
-        {secondButtonText}
+      <button
+        className="question-btn"
+        onClick={() => onButtonClick(optionBText)}>
+        {optionBText}
       </button>
       <span className="question-text">?</span>
     </div>
   );
 }
 
-function Response({ selection }) {
+function Response({ optionAText, optionBText, selection }) {
   return (
     <div className="response-container">
-      {selection && <p className="response-text">You selected: <span>{selection}</span></p>}
+      <p className="response-text">You selected: {selection && <span>{selection}</span>}</p>
+      {selection && <p className="response-desciption">You are someone who prefers {selection === optionAText ? selection : optionBText} over {selection === optionAText ? optionBText : optionAText}.</p>}
     </div>
   );
 }
 
 function App() {
+  const randomNumber = Math.floor(Math.random() * data.length);
+  const [optionAText, setOptionAText] = useState(data[randomNumber].optionA);
+  const [optionBText, setOptionBText] = useState(data[randomNumber].optionB);
   const [selected, setSelected] = useState(null);
-  // const [response, setResponse] = useState("Dog");
 
   function handleButtonClick(buttonText) {
-    // console.log("clicked")
     setSelected(buttonText);
-    console.log(selected);
-
   }
 
   return (
     <div>
       <div className="App">
         <Navbar />
-        <Question onButtonClick={handleButtonClick} selection={selected} />
-        <Response selection={selected} />
+        <Question
+          optionAText={optionAText}
+          optionBText={optionBText}
+          selection={selected}
+          onButtonClick={handleButtonClick}
+        />
+        <Response
+          optionAText={optionAText}
+          optionBText={optionBText}
+          selection={selected}
+        />
       </div>
     </div>
   );
